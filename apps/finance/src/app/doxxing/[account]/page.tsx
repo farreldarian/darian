afe1469a8fec625f474a8d96cd762ec663bdfbe7
farreldarian <farreldarian@users.noreply.github.com>
2023-10-20 16:58:16 +0700

    List all active chains

--- apps/finance/src/app/doxxing/[account]/page.tsx
+++ apps/finance/src/app/doxxing/[account]/page.tsx
@@ -1,9 +1,33 @@
 import { Address } from 'viem'
+import { arbitrum, avalanche, bsc, mainnet, polygon } from 'viem/chains'
+import { getWalletActiveChains } from '~/lib/moralis/get-wallet-active-chains'
+import { getMoralis } from '~/lib/moralis/moralis'
 
 type Props = {
   params: { account: Address }
 }
 
-export default function Page(props: Props) {
-  return <main>{props.params.account}</main>
+export default async function Page(props: Props) {
+  const chains = await fetchActiveChains(props.params.account)
+
+  return (
+    <main>
+      <h1>{props.params.account}</h1>
+      <ol>
+        {chains.map((chain) => (
+          <li key={chain.chain_id}>{chain.chain}</li>
+        ))}
+      </ol>
+    </main>
+  )
+}
+
+async function fetchActiveChains(account: Address) {
+  return getWalletActiveChains(getMoralis(), {
+    account,
+    chains: [mainnet.id, polygon.id, bsc.id, avalanche.id, arbitrum.id],
+  }).then((chains) =>
+    // Should have first transaction
+    chains.active_chains.filter((chain) => chain.first_transaction != null)
+  )
 }

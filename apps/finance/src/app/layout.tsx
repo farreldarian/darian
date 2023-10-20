b035b80adcf46b6647cfb801a2700b3a0da658fa
farreldarian <farreldarian@users.noreply.github.com>
2023-10-20 18:01:38 +0700

    Install theme

--- apps/finance/src/app/layout.tsx
+++ apps/finance/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
-import { cn } from '~/lib/utils'
+import { ThemeProvider } from '~/components/theme-provider'
 import './globals.css'
 
 export const fontSans = FontSans({
@@ -20,13 +20,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body
-        className={cn(
-          'min-h-[100svh] bg-background font-sans antialiased',
-          fontSans.variable
-        )}
-      >
-        {children}
+      <body className={fontSans.variable}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )

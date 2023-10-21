05b8b18c186811140fbdc757e9a42c9fe0c39dfd
farreldarian <farreldarian@users.noreply.github.com>
2023-10-21 17:50:58 +0700

    Use buttonGroup recipe

--- apps/finance/src/app/doxxing/page.tsx
+++ apps/finance/src/app/doxxing/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '~/components/ui/button'
+import { buttonGroup } from '~/components/ui/button/recipes'
+import { cn } from '~/lib/utils'
 import { AddressInput, DoxxingForm } from './components/doxxing-form'
 
 export default function Page() {
   return (
     <main className='grid min-h-screen place-items-center'>
-      <DoxxingForm className='flex w-full max-w-xs flex-col gap-4'>
+      <DoxxingForm className={cn(buttonGroup(), 'w-full max-w-xs')}>
         <AddressInput autoFocus placeholder='Enter address...' />
         <Button>Submit</Button>
       </DoxxingForm>

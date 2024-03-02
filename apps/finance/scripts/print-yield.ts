976c386a3190dc314ff8b90a96ae54e1ed200d65
farreldarian <farreldarian@users.noreply.github.com>
2024-03-02 10:05:15 +0700

    feat: update

--- apps/finance/scripts/print-yield.ts
+++ apps/finance/scripts/print-yield.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+import { Decimal } from 'decimal.js'
 import { reverse, zip } from 'lodash'
 import { formatUnits } from 'viem'
 import { arbitrum } from 'viem/chains'
@@ -7,8 +9,11 @@ import { getArbitrumClient } from '~/lib/viem/get-public-client'
 
 const chain = arbitrum
 const publicClient = getArbitrumClient()
-const account = '0x'
-const token = '0x'
+const account = '0xbcebf81faad87ce9e1c630e2993734a20183e1d3'
+// USDC.e
+// const token = '0x625E7708f30cA75bfd92586e17077590C60eb4cD'
+// USDC
+const token = '0x724dc807b04555b71ed48a6896b6F41593b8C637'
 
 console.time('Fetching blocks')
 const blocks = reverse(await getLastNDaysBlocks(chain.id, 93))
@@ -23,11 +28,15 @@ const balances = await getBalancesOnBlocks({
 })
 console.timeEnd('Fetching balances')
 
-console.log('Summary:')
-console.log(getSummary())
+console.log('Daily Summary:')
+const dailySummary = getDailySummary()
+console.log(dailySummary)
+
+console.log('Monthly Summary:')
+console.log(getMonthlySummary(dailySummary))
 
 // #region Helpers
-function getSummary() {
+function getDailySummary() {
   if (balances.length !== blocks.length) throw new Error('length mismatch')
   return zip(balances, blocks).map(([balance, block], i, arr) => {
     balance = balance!
@@ -41,7 +50,11 @@ function getSummary() {
     return {
       date: block.date,
       balance: formatUnits(balance, 6),
-      earnedForADay: (() => {
+      earned:
+        earnedForADay == null
+          ? new Decimal(0)
+          : new Decimal(formatUnits(earnedForADay, 6)),
+      earned_fmt: (() => {
         if (earnedForADay == null) return '-'
         const day = formatUnits(earnedForADay, 6)
         const month = formatUnits(earnedForADay * 31n, 6)
@@ -61,6 +74,22 @@ function getSummary() {
   })
 }
 
+function getMonthlySummary(dailySummary: ReturnType<typeof getDailySummary>) {
+  const map = new Map<string, { earned: Decimal }>()
+  for (const daily of dailySummary) {
+    const key = format(daily.date, 'yyyy-MM')
+    const data = map.get(key) ?? { earned: new Decimal(0) }
+
+    data.earned = daily.earned.add(data.earned)
+
+    map.set(key, data)
+  }
+
+  return Array.from(map.entries()).map(([date, data]) => {
+    return { date, earned: data.earned.toString() }
+  })
+}
+
 function compactUsd(value: string) {
   return Number(value).toLocaleString(undefined, {
     style: 'currency',
@@ -69,3 +98,5 @@ function compactUsd(value: string) {
   })
 }
 // #endregion
+
+process.exit(0)
